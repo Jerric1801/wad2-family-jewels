664804ae87652a3737c1d86d3d536656201cfd61
Jerric1801 <Jerric1801@gmail.com>
2024-09-16 13:06:27 +0800

    add firebase functions

--- src/services/firebase/auth.js
+++ src/services/firebase/auth.js
@@ -9,18 +9,20 @@ export async function handleLogin(email, password) {
         const q = query(usersCollection, where("email", "==", email));
         const querySnapshot = await getDocs(q);
         const user = querySnapshot.docs[0].data();
-        const passwordMatch = await comparePassword(
-            password,
-            user.password
-        );
-        if (passwordMatch) {
-            return true
-        } else {
-            this.errorMessage = "Invalid email or password";
+        const passwordMatch = await comparePassword(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error('Invalid password. Please try again.');
         }
+
+        return user;
     } catch (error) {
-        console.error("Error during login:", error);
-        throw error;
+        if (error.code === 'auth/user-not-found') {
+            throw new Error('No account found with that email.');
+        } else {
+            console.error("Error during login:", error);
+            throw new Error('An error occurred during login. Please try again later.');
+        }
     }
 }
 
@@ -28,14 +30,18 @@ export async function handleSignUp(name, email, password) {
     try {
         const hashedPassword = await hashPassword(password);
         await addDoc(collection(db, "accounts"), {
-          name: name,
-          email: email,
-          password: hashedPassword,
+            name: name,
+            email: email,
+            password: hashedPassword,
         });
 
         return true
     } catch (error) {
-        console.error("Error during signup:", error);
-        throw error;
+        if (error.code === 'auth/email-already-in-use') {
+            throw new Error('Email is already in use. Please try a different one or log in.');
+        } else {
+            console.error("Error during signup:", error);
+            throw new Error('An error occurred during signup. Please try again later.');
+        }
     }
 }
