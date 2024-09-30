be677154b3e26b83aec32c75bc9daefbb087a931
Jerric1801 <Jerric1801@gmail.com>
2024-09-30 17:53:06 +0800

    editor

---
+++ src/components/jewelry/Editor.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-[700px] h-[350px] flex">
+        <div class="w-[10%] h-full">
+            
+        </div>
+        <div class="w-[60%] h-full">
+            <img :src=imgSrc alt="Product Image">
+        </div>
+        <div class="w-[30%] h-full flex flex-col">
+            <div class="w-[100%] bg-grey flex justify-center items-center">
+                <p>Toolbar</p>
+            </div>
+            <div class="">
+                <p>Selected Product</p>
+                <p>Product Name</p>
+                <img :src=productSrc alt="Product Image">
+                <p>Select Another Product</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Editor',
+    props: {
+        imgSrc: String,
+        productSrc: String
+    },
+    data() {
+    }
+}
+
+
+</script>
--- src/views/jewelry/ModelSelectionView.vue
+++ src/views/jewelry/ModelSelectionView.vue
@@ -1,3 +1,26 @@
 <template>
+    <DefaultLayout>
+        <Editor :img-src=mainImg :productSrc=productImg /> 
+    </DefaultLayout>
 </template>
+<script>
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import Editor from "@/components/jewelry/Editor.vue"
+import product1 from '@/assets/images/home/product_2.jpg';
+import product2 from '@/assets/images/home/product_3.png';
 
+export default {
+    name: "ModelSelectionView",
+    components: {
+        DefaultLayout,
+        Editor
+    },
+    data() {
+        return {
+            mainImg: product1,
+            productImg: product2
+        }
+    }
+
+}
+</script>
