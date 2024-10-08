7098df71fefceb4120c184684dfb60eadeab136e
Chicoo000 <110173045+Chicoo000@users.noreply.github.com>
2024-10-08 21:53:20 +0800

    Update JewelryUploadView.vue

--- src/views/jewelry/JewelryUploadView.vue
+++ src/views/jewelry/JewelryUploadView.vue
@@ -2,20 +2,18 @@
     <DefaultLayout>
         <div class="container mx-auto px-4 pt-[150px]">
 
-            <div class="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 ">
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
                 <div>
-                    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 gap-6 overflow-y-auto">
-
-                        <div class="lg:w-40 lg:w-40 sm:lg-20 rounded shadow-lg sm:w-20" v-for="image in images">
+                    <div class="h-3/4 col-span-1 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1  grid grid-cols-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 overflow-y-auto">
+                        <div class="w-42  rounded " v-for="image in images">
                             <img class="w-full h-40" :src="image" alt="">
-
                         </div>
                     </div>
 
                 </div>
-                <div class="container px-6" id="uploadDiv">
-                    <div class="rounded shadow-lg bg-gray-200 lg:h-full lg:w-64 mx-auto content-center" >
-                        <img class = "lg:w-64 lg:h-full" id="mainImg" :src="mainImg" v-if="mainImg != ''">
+                <div class="container col-span-2" id="uploadDiv">
+                    <div class="rounded shadow-lg bg-gray-200 lg:h-96 lg:w-3/4 mx-auto content-center" >
+                        <img class = "lg:w-full lg:h-full" id="mainImg" :src="mainImg" v-if="mainImg != ''">
                         <div v-else class="bg-grey-200">
                             <p class="text-center border-2 p-8">No Image Selected</p>
                         </div>
@@ -34,6 +32,10 @@
                             </span>
                             Upload Image
                         </button>
+                        <div v-if="error" style="text-align: center;" >
+                            <p style="color:red" >{{ errorMsg }}</p>
+                        </div> 
+
                     </div>
                 </div>
             </div>
@@ -42,9 +44,7 @@
     </DefaultLayout>
 </template>
 <style >
-#uploadDiv{
-    max-height:50%
-}
+
 </style>
 <script>
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
@@ -57,13 +57,19 @@ export default {
     data() {
         return {
             images: images,
-            mainImg: ''
+            mainImg: '',
+            error: false,
+            errorMsg: '',
 
         };
     },
     methods: {
         uploadImage() {
-            const input = document.createElement('input');
+            if(this.images.length == 2){
+                this.error = true;
+                this.errorMsg = 'You can only upload 6 images';
+            }else{
+                const input = document.createElement('input');
             input.type = 'file';
             input.onchange = e => {
                 const file = e.target.files[0];
@@ -76,6 +82,8 @@ export default {
                 }
             }
             input.click();
+            }
+            
         }
     }
 }
