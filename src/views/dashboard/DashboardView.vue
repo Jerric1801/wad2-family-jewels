802a0fe786cc6cd26ac67908eb920c257e8fa14f
isabellegend <isabel.lee.2023@scis.smu.edu.sg>
2024-10-13 21:22:29 +0800

    Update DashboardView.vue

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,2 +1,8 @@
 <template>
+    <DefaultLayout>
+    <div class="container mx-auto px-4 pt-[150px]">
+      <h1 class="text-3xl font-bold mb-8 animate-fade-in">Dashboard</h1>
+      
+    </div>
+  </DefaultLayout>
 </template>
