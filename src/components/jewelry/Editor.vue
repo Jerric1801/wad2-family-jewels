baf6d58cdde3543e3125b5c49b2ff860d870ba54
Jerric1801 <Jerric1801@gmail.com>
2024-10-16 19:34:30 +0800

    💎 layout for image-generation page

--- src/components/jewelry/Editor.vue
+++ src/components/jewelry/Editor.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="w-[700px] h-[350px] flex">
-        <div class="w-[10%] h-full">
-            
-        </div>
-        <div class="w-[60%] h-full">
-            <img :src=imgSrc alt="Product Image">
-        </div>
-        <div class="w-[30%] h-full flex flex-col">
-            <div class="w-[100%] bg-grey flex justify-center items-center">
-                <p>Toolbar</p>
+    <div class="w-[100%] h-[100%] flex bg-white rounded-md">
+        <div class="w-[15%] h-full flex flex-col items-center space-y-4 justify-end">
+            <div class="w-[90%]">
+                <input type="range" min="1" :max="maxZoom" v-model="zoomLevel" orientation="vertical" />
             </div>
-            <div class="">
-                <p>Selected Product</p>
-                <p>Product Name</p>
-                <img :src=productSrc alt="Product Image">
+        </div>
+        <div class="w-[55%] h-full flex justify-center items-center">
+            <div class="w-[90%] h-[90%] relative overflow-hidden bg-grey flex items-center rounder-md">
+                <img :src="imgSrc" alt="Product Image" class="absolute" :style="zoomStyle">
+            </div>
+        </div>
+        <div class="w-[30%] h-full flex flex-col items-center">
+            <div class="h-[25%] w-[90%] bg-grey flex flex-col justify-start items-center rounded-md mt-[10%]">
+                <p class="mt-2"><b>Toolbar</b></p>
+                <p class="mt-2">Opacity</p>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <input type="range" min="0" max="1" step="0.1" v-model="opacity" />
+                </div>
+            </div>
+            <div class="h-[60%] w-[90%] bg-grey flex flex-col justify-start items-center rounded-md mt-[5%]">
+                <p class="mt-2"><b>Selected Product</b></p>
+                <p class="mt-2">Product Name</p>
+                <div class="w-[50%] h-[50%] relative overflow-hidden bg-grey flex items-center">
+                    <img :src=productSrc alt="Product Image" class="absolute object-cover">
+                </div>
                 <p>Select Another Product</p>
             </div>
         </div>
@@ -27,8 +37,22 @@ export default {
         productSrc: String
     },
     data() {
+        return {
+            zoomLevel: 2,
+            maxZoom: 10,
+            opacity: 1
+        };
+    },
+    computed: {
+        zoomStyle() {
+            return {
+                transform: `scale(${this.zoomLevel})`,
+                transformOrigin: 'center',
+                maxWidth: '100%',
+                maxHeight: '100%',
+                opacity: this.opacity
+            };
+        }
     }
 }
-
-
 </script>
--- src/views/jewelry/ModelSelectionView.vue
+++ src/views/jewelry/ModelSelectionView.vue
@@ -1,6 +1,22 @@
 <template>
     <DefaultLayout>
-        <Editor :img-src=mainImg :productSrc=productImg /> 
+        <div class="bg-white h-[120vh] w-[100vw] flex flex-row justify-center relative p-[20px]">
+            <div class="w-[27.5%] mt-[20vh] flex justify-center h-[90vh]">
+                <div class="w-[95%] flex flex-col items-center bg-grey rounded-md">
+
+                </div>
+            </div>
+            <div class="w-[72.5%] mt-[20vh] flex flex-col h-[90vh] justify-between">
+                <div class="w-full h-[62.5%] overflow-hidden">
+                    <Editor :img-src=mainImg :productSrc=productImg />
+                </div>
+                <div class="w-full h-[35%]">
+                    <div class="w-[100%] h-full flex flex-col items-center bg-grey rounded-md">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </DefaultLayout>
 </template>
 <script>
